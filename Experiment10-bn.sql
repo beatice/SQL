ea63b938d52766cd424fcb3d583600ecec4e6dea
beatice <1979500428@qq.com>
2018-06-27 14:46:15 +0800

    Update Experiment10-bn.sql

--- Experiment10-bn.sql
+++ Experiment10-bn.sql
@@ -4,7 +4,7 @@ SELECT TRUE, true, FALSE, false;
 
 # NULL Values
 /* The NULL value means “no data.” NULL can be written in any lettercase. A synonym is \N (case-sensitive). */
-SELECT NULL,null,'\N','\n';
+SELECT NULL, null, '\N', '\n';
 
 /* the NULL value is different from values such as 0 for numeric types or the empty string for string types.  */
 SELECT NULL=0, NULL='';
